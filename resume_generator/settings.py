5cc90d4ccf4648a8c745de1a8b5e580a9ed3c0bc
Jeet8204 <jeetmukherjee8204@gmail.com>
2025-07-14 13:30:43 +0530

    degugging

--- resume_generator/settings.py
+++ resume_generator/settings.py
@@ -163,11 +163,8 @@ SOCIALACCOUNT_PROVIDERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # if your CSS is in static/pdf/
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
